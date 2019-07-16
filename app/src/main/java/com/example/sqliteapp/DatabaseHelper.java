4684d9dcc6b3194d30224caa98136ee36ed2162f
oexlye08 <52402554+oexlye08@users.noreply.github.com>
2019-07-16 20:08:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sqliteapp/DatabaseHelper.java
@@ -0,0 +1,26 @@
+package com.example.sqliteapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static String DATABASE_NAME="student_data";
+    public static int DATABASE_VERSION=1;
+
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME,null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
